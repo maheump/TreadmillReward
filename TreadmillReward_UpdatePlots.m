e7e489f1118439e4598d085607b32437ce0f93cd
Maxime Maheu <maheu.mp@gmail.com>
2022-05-25 20:17:39 +0200

    Also update the live plot of total reward intake

--- TreadmillReward_UpdatePlots.m
+++ TreadmillReward_UpdatePlots.m
@@ -6,19 +6,20 @@ function Outcomes = TreadmillReward_UpdatePlots(S, TrialTypes, Outcomes, iRewLoc
 % Declare global variable which can be accessed outside this function
 global BpodSystem;
 
+% In the automatic reward delivery method vs. regular experiment, adapt to
+% different state names corresponding to mouse licking water
+if S.GUI.Automatic_reward_delivery
+    field = BpodSystem.Data.RawEvents.Trial{iRewLoc}.States.WaterCollected;
+elseif ~S.GUI.Automatic_reward_delivery
+    field = BpodSystem.Data.RawEvents.Trial{iRewLoc}.States.DetectedRewardedLicks;
+end
+
 % Update the amount of reward given to the animal
-NumOfRewardDeliveries = sum(~isnan(BpodSystem.Data.RawEvents.Trial...
-    {iRewLoc}.States.DetectedRewardedLicks(1:2:end)));
+NumOfRewardDeliveries = sum(~isnan(field(1:2:end)));
 RewardAmount = NumOfRewardDeliveries * S.GUI.Reward_amount;
 TotalRewardDisplay('add', RewardAmount);
 
 % Update the outcome plot
-CurrentTrial = BpodSystem.Data.RawEvents.Trial{iRewLoc};
-if S.GUI.Automatic_reward_delivery
-    field = CurrentTrial.States.WaterCollected;
-elseif ~S.GUI.Automatic_reward_delivery
-    field = CurrentTrial.States.DetectedRewardedLicks;
-end
 NumOfRewardDeliveries = sum(~isnan(field(1:2:end)));
 Outcomes(iRewLoc) = double(NumOfRewardDeliveries > 0);
 TrialTypeOutcomePlot(BpodSystem.GUIHandles.OutcomePlot, ...
